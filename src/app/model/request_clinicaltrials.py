66e210e5fb86c8c9b7c0d81e4b949020e3b1f97c
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2023-10-30 11:32:43 +0300

    feat(ct-api): params and request items with helper comments

--- src/app/model/request_clinicaltrials.py
+++ src/app/model/request_clinicaltrials.py
@@ -1,12 +1,58 @@
 from pydantic import BaseModel
 
 
-class RequestClinicalTrial(BaseModel):
-    # T ODO.1
+class RequestParamsClinicalTrial(BaseModel):
+
+    request_params = {
+        "pageSize": 10,  # Sayfa başına sonuç sayısı
+        "pageToken": None,  # İlk sayfada bu parametre boş olmalıdır
+        "query.cond": "cancer",
+        "filter.overallStatus": "RECRUITING"
+    }
+
+
+class RequestStudiesClinicalTrial(BaseModel):
     """
-    from clinicaltrials.gov/data-about-studies section
-    PieceName and their related request params defined
-    for api/v2/studies,
-    each related params should be define under that.
-    took their reference from response body
+    studies: list of dictionaries
+             each studies had 3 dictionaries
+             these key of main dictionaries is
+                protocolSection, data type is dictionary had nested 12 dictionaries
+                derivedSection, data type is dictionary  had nested 3 dictionaries
+                hasResults, data type is bool
+
+                in protocolSection,
+                    identification module
+                        studies[each]["protocolSection"]["identificationModule"]["nctId"]: str
+                        studies[each]["protocolSection"]["identificationModule"]["briefTitle"]: str
+                        studies[each]["protocolSection"]["identificationModule"]["officialTitle"]: str
+
+                    status module
+                        studies[each]["protocolSection"]["statusModule"]["statusVerifiedDate"]: str
+                        studies[each]["protocolSection"]["statusModule"]["overallStatus"]: str
+
+                    description module
+                        studies[each]["protocolSection"]["descriptionModule"]["briefSummary"]: str
+                        studies[each]["protocolSection"]["descriptionModule"]["detailedDescription"]: str
+
+                    conditional module
+                        studies[each]["protocolSection"]["conditionalModule"]["conditions"]: list
+                        studies[each]["protocolSection"]["descriptionModule"]["detailedDescription"]: str
+
+                    design module
+                        studies[each]["protocolSection"]["designModule"]["studyType"]: str
+                        studies[each]["protocolSection"]["designModule"]["phases"]: list
+                        studies[each]["protocolSection"]["designModule"]["designInfo"]["allocation"]: str
+                        studies[each]["protocolSection"]["designModule"]["designInfo"]["interventionModel"]: str
+                        studies[each]["protocolSection"]["designModule"]["designInfo"]["primaryPurpose"]: str
+                        studies[each]["protocolSection"]["designModule"]["designInfo"]["maskingInfo"]["masking"]: str
+                        studies[each]["protocolSection"]["designModule"]["designInfo"]["enrollmentInfo"]["count"]: int
+                        studies[each]["protocolSection"]["designModule"]["designInfo"]["enrollmentInfo"]["type"]: str
+
+                    eligibilty module
+                        studies[each]["protocolSection"]["eligibilityModule"]["eligibilityCriteria"]: str
+                        studies[each]["protocolSection"]["eligibilityModule"]["healthyVolunteers"]: bool
+                        studies[each]["protocolSection"]["eligibilityModule"]["sex"]: bool
+                        studies[each]["protocolSection"]["eligibilityModule"]["minimumAge"]: str
+                        studies[each]["protocolSection"]["eligibilityModule"]["stdAges"]: list
     """
+    studies: list
