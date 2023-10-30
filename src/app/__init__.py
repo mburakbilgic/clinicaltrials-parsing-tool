778e77024794c3d9ff9efaeb8a0248600c545d3b
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2023-10-30 15:58:04 +0300

    feat(ct-api): base system

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,13 +1,23 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.openapi.models import OpenAPI
 import yaml
+
+from src.app.worker.worker_main import Worker
 
 os.chdir(".")
 
 MAIN_PATH = os.getcwd()
 CONFIG_PATH = os.path.join(MAIN_PATH, "..\\config\\ctg-oas-v2.yaml")
+DATA_FILES = os.path.join(MAIN_PATH, "..\\data")
+LOG_FILES = os.path.join(MAIN_PATH, "..\\logs")
+
+router = APIRouter()
+
+raw_clinicaltrials = Worker().func_mapping_clinicaltrials
+
+log_call = Worker().func_log
 
 with open(CONFIG_PATH, "r") as file:
     openapi_config = yaml.safe_load(file)
