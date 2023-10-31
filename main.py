4e3d0e37ce831c3c658368cb108ce476207e67cd
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2023-11-01 00:24:31 +0300

    refactor(ct-api): commentary

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 from fastapi import FastAPI
 
-from src.controller import controller_clinicaltrials
+from src.controller import controller_clinicaltrials, controller_log
 
 application = FastAPI()
 
 application.include_router(controller_clinicaltrials.router)
-
-"""
-in controller section; below app.get should be like this;
-import requests
-
-@router.get("/api/v2/studies")
-async def [function_name]
-    ...
-    ...
-    return data
-
-### this below should be change to above
-@app.get("/api/v2/studies")
-def get_studies():
-    response = requests.get(api_url, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        studies = data["studies"]
-        return studies
-    else:
-        return {"error": "İstek başarısız"}"""
+# application.include_router(controller_log.router)
 
 if __name__ == "__main__":
     import uvicorn
