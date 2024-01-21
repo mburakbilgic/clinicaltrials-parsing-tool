81609746068034593ef9a3add4902d8179c7ac82
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2024-01-21 14:08:22 +0300

    feat(gcs): config init implemented

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,7 @@ from fastapi.openapi.models import OpenAPI
 
 MAIN_PATH = os.getcwd()
 CONFIG_PATH = os.path.join(MAIN_PATH, "system_docs\\config\\ctg-oas-v2.yaml")
+GCS_CONFIG_PATH = os.path.join(MAIN_PATH, "system_docs\\config\\monolith-technology-83cdc098a399.json")
 DATA_FILES = os.path.join(MAIN_PATH, "system_docs\\data")
 LOG_FILES = os.path.join(MAIN_PATH, "system_docs\\logs")
 
@@ -15,6 +16,9 @@ router = APIRouter()
 # OpenAPI, Request URL and Query Parameters settings
 with open(CONFIG_PATH, "r") as file:
     openapi_config = yaml.safe_load(file)
+
+# Google SDK Credential Configuration
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(GCS_CONFIG_PATH)
 
 local_app = FastAPI()
 local_app.openapi = OpenAPI(**openapi_config)
@@ -30,7 +34,6 @@ params = {
     "pageSize": 1000,  # Results of each page
     "pageToken": None,  # In first page this should be empty
     "query.cond": "cancer",
-    "filter.overallStatus": "RECRUITING",
-    "filter.studyType": "INTERVENTIONAL"
+    "filter.overallStatus": "RECRUITING"
 }
 response = requests.get(api_url, params=params)
